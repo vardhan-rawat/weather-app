a388a550bf46d10b456c77296c8e73901deb09c8
vardhan-rawat <vardhanrawat1@gmail.com>
2024-01-26 15:47:40 +0530

    added files

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'http://api.positionstack.com/v1/forward?access_key=fa9a16dcdeec365bc480ec665d2ab410&query='+address
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.data.length==0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.data[0].latitude,
+                longitude: body.data[0].longitude,
+                location: body.data[0].label
+            })
+        }
+    })
+}
+
+module.exports = geocode
