429bdfb25d18d4bfc7971896c178219ab67f143f
vardhan-rawat <vardhanrawat1@gmail.com>
2024-02-05 13:11:34 +0530

    deployment api changes

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,12 @@
 const request = require('request')
+const apiKey = 'fa9a16dcdeec365bc480ec665d2ab410';
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://api.positionstack.com/v1/forward' : 'http://localhost:3000/v1/forward';
+
+const url = `${baseUrl}?access_key=${apiKey}&query=${address}`;
 
 const geocode = (address, callback) => {
-    const url = 'http://api.positionstack.com/v1/forward?access_key=fa9a16dcdeec365bc480ec665d2ab410&query='+address
+    const url = `${baseUrl}?access_key=${apiKey}&query=${address}`;
+
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
