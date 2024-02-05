2492039c60d8c23d94a88f34d0fa4fb23dfb1286
vardhan-rawat <vardhanrawat1@gmail.com>
2024-02-05 12:47:27 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,13 +5,11 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const PORT = process.env.PORT || 3000;
 
 
-
-const publicDirectoryPath = path.join(__dirname, 'public')
-const viewsPath = path.join(__dirname, 'templates/views')
-const partialsPath = path.join(__dirname, 'templates/partials')
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 
 app.set('view engine', 'hbs')
@@ -99,6 +97,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is up on port ${PORT}.`)
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
 })
